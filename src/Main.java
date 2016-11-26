06ffdbf7506cece2fece3796941b8aeaef114302
Rich Cherngchaosil <richer_po@hotmail.com>
2016-11-25 21:36:17 -0800

    Update configurable path

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
+	static String PATH_ALL 					= "FederalistPaper/ALL";
+	static String PATH_MADISON 				= "FederalistPaper/MADISON";
+	static String PATH_HAMILTON 			= "FederalistPaper/HAMILTON";
+	static String PATH_JAY 					= "FederalistPaper/JAY";
+	static String PATH_HAMILTON_AND_MADISON = "FederalistPaper/HAMILTON AND MADISON";
+	static String PATH_HAMILTON_OR_MADISON 	= "FederalistPaper/HAMILTON OR MADISON";
+
 	public static void main(String[] args){
 
 		Scanner reader = new Scanner(System.in);
 		DirectoryParser aDP = new DirectoryParser();
-		while(true){
-			String line = reader.nextLine();
-			aDP.parseDirectory(line);
-		}
+//		while(true){
+//			String line = reader.nextLine();
+//			aDP.parseDirectory(line);
+//		}
+
+		aDP.parseDirectory(PATH_HAMILTON_AND_MADISON);
 	}
 }
